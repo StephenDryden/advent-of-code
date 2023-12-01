fc2ce41137faab33676dacdf091ba8bf1c79c755
Stephen Dryden <stephen.j.dryden@gmail.com>
2023-12-01 15:03:57 +0000

    feature: Day 1 part 2

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"unicode"
 )
 
 const inputLocation = "input.txt"
+
+var numberMap = map[string]int{"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
 func main() {
 
@@ -22,28 +25,38 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Printf("input is :%s\n", scanner.Text())
+		line := scanner.Text()
+		fmt.Printf("input is :%s\n", line)
 
 		code := ""
-		for _, text := range scanner.Text() {
+		//read line by line
+		for i, text := range line {
+			// if rune is decimal digit then add it to the code
 			if unicode.IsDigit(text) {
 				code = code + string(text)
+				continue
 			}
+			// rune is not decimal therefore loop through map one to nine
+			substring := line[i:]
+			for s, i := range numberMap {
+				// if remaining unchecked string contains one to nine and it exists at the start of the substring
+				// add it to the code
+				if substring >= s && strings.Contains(substring, s) && substring[0:len(s)] == s {
+					code = code + fmt.Sprint(i)
+				}
+			}
+
 		}
-		fmt.Printf("code is : %v\n", code)
-		result = result + obtainCalibrationValue(code)
-		fmt.Printf("running total is : %v\n", code)
+		result = result + calculateCalibrationValue(code)
 
 		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
 		}
 		fmt.Printf("final answer is :%v\n", result)
-
 	}
-
 }
 
-func obtainCalibrationValue(code string) int {
+func calculateCalibrationValue(code string) int {
 	r := []rune(code)
 	first := r[0]
 	second := r[len(code)-1]
