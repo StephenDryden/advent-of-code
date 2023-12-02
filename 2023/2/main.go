e639638d516cb146ed47e82b8d75781e28156936
Stephen Dryden <stephen.j.dryden@gmail.com>
2023-12-02 20:12:46 +0000

    feature: Day 2 part 2

--- 2023/2/main.go
+++ 2023/2/main.go
@@ -12,15 +12,16 @@ import (
 const inputLocation = "input.txt"
 
 type game struct {
-	id     int
-	rounds []round
-	valid  bool
+	id           int
+	rounds       []round
+	isValid      bool
+	minimumCubes []cube
 }
 
 type round struct {
-	id    int
-	cubes []cube
-	valid bool
+	id      int
+	cubes   []cube
+	isValid bool
 }
 
 type cube struct {
@@ -47,12 +48,12 @@ func main() {
 			printGame(game)
 		}
 
-		fmt.Printf("And the final answer is: %v\n", calculateAnswer(games))
-
 		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
 		}
 	}
+	fmt.Printf("The answer to part 1 is: %v\n", calculateAnswer(games))
+	fmt.Printf("The answer to part 2 is: %v\n", calculateTotalCubePowers(games))
 }
 
 func parseGame(line string) game {
@@ -61,20 +62,20 @@ func parseGame(line string) game {
 
 	line = strings.ReplaceAll(line, "Game ", "")
 
-	parts := strings.SplitAfter(line, ":")
-	id, err := strconv.Atoi(strings.ReplaceAll(parts[0], ":", ""))
+	splitLine := strings.Split(line, ":")
+	id, err := strconv.Atoi(splitLine[0])
 
 	if err != nil {
 		panic(err)
 	}
 	game.id = id
 
-	rounds := parseRounds(strings.Split(parts[1], ";"))
-	game.rounds = rounds
-	game.valid = true
+	game.rounds = parseRounds(strings.Split(splitLine[1], ";"))
+	game.isValid = true
+	game.minimumCubes = calculateMinimumCubes(game)
 	for _, round := range game.rounds {
-		if !round.valid {
-			game.valid = false
+		if !round.isValid {
+			game.isValid = false
 			break
 		}
 	}
@@ -85,13 +86,11 @@ func parseGame(line string) game {
 func parseRounds(rounds []string) []round {
 	parsedRounds := []round{}
 
-	for roundId, roundInput := range rounds {
-
+	for roundId, roundRaw := range rounds {
 		round := round{}
-		cubes := parseCubes(roundInput)
 		round.id = roundId + 1
-		round.cubes = cubes
-		round.valid = validRound(cubes)
+		round.cubes = parseCubes(roundRaw)
+		round.isValid = validRound(round.cubes)
 		parsedRounds = append(parsedRounds, round)
 	}
 
@@ -103,19 +102,18 @@ func parseCubes(round string) []cube {
 
 	parsedCubes := []cube{}
 	cube := cube{}
-	parsedCubeInput := strings.Split(round, ",")
 
-	for _, draw := range parsedCubeInput {
-		parsedDraw := strings.Split(strings.TrimSpace(draw), " ")
+	for _, cubeRaw := range strings.Split(round, ",") {
+		parsedCube := strings.Split(strings.TrimSpace(cubeRaw), " ")
 
-		count, err := strconv.Atoi(strings.ReplaceAll(parsedDraw[0], " ", ""))
+		count, err := strconv.Atoi(strings.ReplaceAll(parsedCube[0], " ", ""))
 
 		if err != nil {
 			panic(err)
 		}
 
 		cube.count = count
-		cube.colour = parsedDraw[1]
+		cube.colour = parsedCube[1]
 
 		parsedCubes = append(parsedCubes, cube)
 	}
@@ -126,7 +124,7 @@ func printGame(game game) {
 
 	for _, round := range game.rounds {
 		for _, cube := range round.cubes {
-			fmt.Printf("Game %v: Round %v: %v %v dice: Game validity %v\n", game.id, round.id, cube.count, cube.colour, game.valid)
+			fmt.Printf("Game %v: Round %v: %v %v dice: Game validity %v: Minimum %v %v, minimum %v %v, minimum %v %v.\n", game.id, round.id, cube.count, cube.colour, game.isValid, game.minimumCubes[0].colour, game.minimumCubes[0].count, game.minimumCubes[1].colour, game.minimumCubes[1].count, game.minimumCubes[2].colour, game.minimumCubes[2].count)
 		}
 	}
 
@@ -159,10 +157,61 @@ func validRound(cubes []cube) bool {
 func calculateAnswer(games []game) (result int) {
 
 	for _, game := range games {
-		if game.valid {
+		if game.isValid {
 			result = result + game.id
 		}
 	}
 
 	return result
+}
+
+func calculateMinimumCubes(game game) []cube {
+
+	minimumCubes := []cube{}
+
+	blueCube := cube{
+		colour: "blue",
+		count:  0,
+	}
+	greenCube := cube{
+		colour: "green",
+		count:  0,
+	}
+	redCube := cube{
+		colour: "red",
+		count:  0,
+	}
+
+	for _, round := range game.rounds {
+		for _, cube := range round.cubes {
+			switch cube.colour {
+			case "blue":
+				if cube.count > blueCube.count {
+					blueCube.count = cube.count
+				}
+			case "red":
+				if cube.count > redCube.count {
+					redCube.count = cube.count
+				}
+			case "green":
+				if cube.count > greenCube.count {
+					greenCube.count = cube.count
+				}
+			}
+		}
+	}
+
+	minimumCubes = append(minimumCubes, blueCube, greenCube, redCube)
+
+	return minimumCubes
+}
+
+func calculateTotalCubePowers(games []game) int {
+	answer := 0
+	for _, game := range games {
+		printGame(game)
+		answer = answer + (game.minimumCubes[0].count * game.minimumCubes[1].count * game.minimumCubes[2].count)
+	}
+
+	return answer
 }
